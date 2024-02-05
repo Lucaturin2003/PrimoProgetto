bfa4e6243e35cb09a6194b15ee6cac6494510128
Lucaturin2003 <159005233+Lucaturin2003@users.noreply.github.com>
2024-02-05 17:02:56 +0100

    Add files via upload

---
+++ progettoClassi/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace progettoClassi
+{
+    public class Order
+    {
+        
+    }
+}
---
+++ progettoClassi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace progettoClassi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Luca";
+
+            var order = new Order();
+            customer.Orders = new List<Order>();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
